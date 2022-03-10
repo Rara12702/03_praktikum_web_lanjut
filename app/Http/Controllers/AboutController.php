551c28acebb635482a4206de7f3a43a579dc602c
Rara12702 <raradeninda12@gmail.com>
2022-03-10 21:02:03 +0700

    Js 3 prak 1

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index() {
+        echo "KELOMPOK 5 <br> Muhammad Hamamiy Zadah <br> Rara Deninda Hurianto <br> Yanu Miftahul H."
+        ;
+    }
+}
